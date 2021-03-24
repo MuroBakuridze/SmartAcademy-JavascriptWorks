16c874ff1b1e54dedc913e31e8b30d5e46ced6c6
MuroBakuridze <78916692+MuroBakuridze@users.noreply.github.com>
2021-03-24 23:31:23 +0400

    some code

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ var addProductBtb = document.getElementById('add-product')
 var productList = document.getElementById('product-list')
 
 
+var prod;
+var cost;
 
 var allProducts = [
 ]
@@ -13,10 +15,12 @@ var allProducts = [
 var renderAllItem = function () {
   productList.innerText = '';
 
+
   allProducts.forEach(function (item) {
     var itemLi = document.createElement('li')
     itemLi.innerText = item
     productList.appendChild(itemLi)
+
   })
   productInput.value = '';
   costInput.value = '';
@@ -25,8 +29,23 @@ var renderAllItem = function () {
 var handleButtonClick = () => {
   var inputValue = productInput.value
   var costValue = costInput.value
-  allProducts.push(inputValue, costValue)
-  renderAllItem()
+  allProducts.push({prod: inputValue, cost:costValue})
+  renderAllItem(allProducts=[{prod: inputValue,cost:costValue}])
+  // renderAllItem(prod,cost)
+  console.log(allProducts)
+  console.log(inputValue,costValue)
 }
 
+
+
 addProductBtb.addEventListener('click', handleButtonClick)
+
+var finalResultBtn = document.getElementById('final-result')
+var totalCost = document.getElementById('total-cost')
+
+var numbers = allProducts
+
+
+finalResultBtn.addEventListener('click', () => {
+  totalCost.innerText = numbers.reduce((total, current) => total + current, 0)
+})
