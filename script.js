5c8668ef658e91f7ada72166ac4e66ad6e141087
MuroBakuridze <78916692+MuroBakuridze@users.noreply.github.com>
2021-03-30 17:02:53 +0400

    added html, css and js Calc

---
+++ script.js
@@ -0,0 +1,24 @@
+const inputCalc = document.querySelector('.account'),
+      resultCalc = document.querySelector('.result');
+
+
+function input(i) {
+  inputCalc.value = inputCalc.value + i
+}
+
+function result() {
+  if(eval(inputCalc.value) == undefined){
+    resultCalc.value = '0'
+    inputCalc.value = ''
+  }if(eval(inputCalc.value) == Infinity){
+    resultCalc.value = '0'
+    inputCalc.value = 'You think I\'m Stuped? :P'
+  }
+  resultCalc.value = eval(inputCalc.value)
+  inputCalc.value = eval(inputCalc.value)
+}
+
+function reset() {
+  resultCalc.value = '0'
+  inputCalc.value = ''
+}
